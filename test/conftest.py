d3afb926c34103459255c9736f103d6ac1b8d30a
Richard Edgar <riedgar@microsoft.com>
2022-01-06 09:54:59 -0500

    Copy test source
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

---
+++ test/conftest.py
@@ -0,0 +1,40 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+import json
+import pytest
+
+from azure.identity import DefaultAzureCredential
+from azure.ml import MLClient
+
+
+@pytest.fixture(scope='session')
+def component_config():
+    config_file = 'component_config.json'
+
+    with open(config_file, 'r') as cf:
+        result = json.load(cf)
+
+    return result
+
+@pytest.fixture(scope='session')
+def workspace_config():
+    ws_config_file = 'config.json'
+
+    with open(ws_config_file) as cf:
+        result = json.load(cf)
+
+    return result
+
+@pytest.fixture(scope='function')
+def ml_client(workspace_config):
+    client = MLClient(
+        credential=DefaultAzureCredential(),
+        subscription_id=workspace_config['subscription_id'],
+        resource_group_name=workspace_config['resource_group'],
+        workspace_name=workspace_config['workspace_name'],
+        logging_enable=True
+    )
+
+    return client
