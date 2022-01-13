db0a0e1a66ed50b54b2d0be9f73a6f33c4e810c4
Richard Edgar <riedgar@microsoft.com>
2022-01-13 08:44:13 -0500

    Finish off the script
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- scripts/generate_registration_files.py
+++ scripts/generate_registration_files.py
@@ -16,9 +16,19 @@ def prompt_yes_no(prompt: str) -> bool:
 def create_workspace_config():
     JSON_FILE = "config.json"
     config_dict = dict()
-    config_dict["subscription_id"] = input("Enter subscription id")
-    config_dict["resource_group"] = input("Enter resource group name")
-    config_dict["workspace_name"] = input("Enter workspace name")
+    config_dict["subscription_id"] = input("Enter subscription id: ")
+    config_dict["resource_group"] = input("Enter resource group name: ")
+    config_dict["workspace_name"] = input("Enter workspace name: ")
+
+    with open(JSON_FILE, "w") as jf:
+        json.dump(config_dict, jf)
+    print("Written {0}".format(JSON_FILE))
+
+
+def create_component_config():
+    JSON_FILE = "component_config.json"
+    config_dict = dict()
+    config_dict["version"] = int(input("Enter version: "))
 
     with open(JSON_FILE, "w") as jf:
         json.dump(config_dict, jf)
@@ -26,8 +36,11 @@ def create_workspace_config():
 
 
 def main():
-    if prompt_yes_no("Create workspace config.json?"):
+    if prompt_yes_no("Create workspace config.json? "):
         create_workspace_config()
+    if prompt_yes_no("Create component_config.json? "):
+        create_component_config()
+    print("Completed")
 
 
 if __name__ == "__main__":
