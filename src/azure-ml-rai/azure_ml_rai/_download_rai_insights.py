14d3a0be4997ca60f64029782ae4b148c8d86c82
Richard Edgar <riedgar@microsoft.com>
2022-01-18 11:43:18 -0500

    Change auth procedure for download
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- src/azure-ml-rai/azure_ml_rai/_download_rai_insights.py
+++ src/azure-ml-rai/azure_ml_rai/_download_rai_insights.py
@@ -19,7 +19,7 @@ from azure.storage.blob import BlobServiceClient
 from azure.ml import MLClient
 
 from ._constants import OutputPortNames
-from ._utilities import _get_v1_workspace_client
+from ._utilities import _get_v1_workspace_client, _get_storage_account_key
 
 
 def _get_output_port_info(
@@ -37,26 +37,29 @@ def _get_output_port_info(
 
 
 def _download_port_files(
-    mlflow_client: MlflowClient,
+    ml_client: MLClient,
     run_id: str,
     port_name: str,
     target_directory: Path,
     credential: ChainedTokenCredential,
 ) -> None:
+    mlflow_client = MlflowClient()
     port_info = _get_output_port_info(mlflow_client, run_id, port_name)
 
     wasbs_tuple = AzureBlobArtifactRepository.parse_wasbs_uri(port_info["Uri"])
-    storage_account = wasbs_tuple[1]
+    storage_account_name = wasbs_tuple[1]
     if len(wasbs_tuple) == 4:
         account_dns_suffix = wasbs_tuple[3]
     else:
         account_dns_suffix = "blob.core.windows.net"
 
     account_url = "https://{account}.{suffix}".format(
-        account=storage_account, suffix=account_dns_suffix
+        account=storage_account_name, suffix=account_dns_suffix
     )
 
-    bsc = BlobServiceClient(account_url=account_url, credential=credential)
+    sa_key = _get_storage_account_key(ml_client, storage_account_name)
+
+    bsc = BlobServiceClient(account_url=account_url, credential=sa_key)
     abar = AzureBlobArtifactRepository(port_info["Uri"], client=bsc)
 
     # Download everything
@@ -68,13 +71,11 @@ def download_rai_insights(ml_client: MLClient, rai_insight_id: str, path: str) -
 
     mlflow.set_tracking_uri(v1_ws.get_mlflow_tracking_uri())
 
-    mlflow_client = MlflowClient()
-
     output_directory = Path(path)
     output_directory.mkdir(parents=True, exist_ok=False)
 
     _download_port_files(
-        mlflow_client,
+        ml_client,
         rai_insight_id,
         OutputPortNames.RAI_INSIGHTS_GATHER_RAIINSIGHTS_PORT,
         output_directory,
