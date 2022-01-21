144ab2955b80cbbf941ca005ba5fe91bd9ca75c7
Richard Edgar <riedgar@microsoft.com>
2022-01-21 09:32:28 -0500

    Try adding an error analysis test with more args
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/rai/test_erroranalysis_component.py
+++ test/rai/test_erroranalysis_component.py
@@ -53,7 +53,7 @@ class TestErrorAnalysisComponent:
             outputs=create_rai_outputs,
         )
 
-        # Setup counterfactual
+        # Setup error analysis
         erroranalysis_inputs = {
             "rai_insights_dashboard": "${{jobs.create-rai-job.outputs.rai_insights_dashboard}}",
             "max_depth": "4",
@@ -82,8 +82,92 @@ class TestErrorAnalysisComponent:
 
         # Pipeline to construct the RAI Insights
         insights_pipeline_job = PipelineJob(
-            experiment_name=f"Counterfactual_Classification_All_Args_{version_string}",
-            description="Expected failure due to multiple tool instances",
+            experiment_name=f"ErrorAnalysis_Classification_All_Args_{version_string}",
+            description="Check error analysis with all arguments",
+            jobs={
+                "fetch-model-job": fetch_job,
+                "create-rai-job": create_rai_job,
+                "erroranalysis-rai-job": erroranalysis_job,
+                "gather-job": gather_job,
+            },
+            inputs=pipeline_inputs,
+            outputs=None,
+            compute="cpucluster",
+        )
+
+        # Send it
+        insights_pipeline_job = submit_and_wait(ml_client, insights_pipeline_job)
+        assert insights_pipeline_job is not None
+
+    def test_regression_all_args(
+        self, ml_client: MLClient, component_config, registered_boston_model_id: str
+    ):
+        version_string = component_config["version"]
+
+        # Pipeline globals
+        pipeline_inputs = {
+            "target_column_name": "income",
+            "my_training_data": JobInput(dataset=f"Boston_Train_PQ:{version_string}"),
+            "my_test_data": JobInput(dataset=f"Boston_Test_PQ:{version_string}"),
+        }
+
+        # The job to fetch the model
+        fetch_job_inputs = {"model_id": registered_boston_model_id}
+        fetch_job_outputs = {"model_info_output_path": None}
+        fetch_job = ComponentJob(
+            component=f"FetchRegisteredModel:{version_string}",
+            inputs=fetch_job_inputs,
+            outputs=fetch_job_outputs,
+        )
+
+        # Top level RAI Insights component
+        create_rai_inputs = {
+            "title": "Run built from Python",
+            "task_type": "regression",
+            "model_info_path": "${{jobs.fetch-model-job.outputs.model_info_output_path}}",
+            "train_dataset": "${{inputs.my_training_data}}",
+            "test_dataset": "${{inputs.my_test_data}}",
+            "target_column_name": "${{inputs.target_column_name}}",
+            "categorical_column_names": "[]",
+        }
+        create_rai_outputs = {"rai_insights_dashboard": None}
+        create_rai_job = ComponentJob(
+            component=f"RAIInsightsConstructor:{version_string}",
+            inputs=create_rai_inputs,
+            outputs=create_rai_outputs,
+        )
+
+        # Setup error analysis
+        erroranalysis_inputs = {
+            "rai_insights_dashboard": "${{jobs.create-rai-job.outputs.rai_insights_dashboard}}",
+            "max_depth": "4",
+            "num_leaves": "25",
+            "min_child_samples": "10",
+            "filter_features": '[]',
+        }
+        erroranalysis_outputs = {"error_analysis": None}
+        erroranalysis_job = ComponentJob(
+            component=f"RAIInsightsErrorAnalysis:{version_string}",
+            inputs=erroranalysis_inputs,
+            outputs=erroranalysis_outputs,
+        )
+
+        # Configure the gather component
+        gather_inputs = {
+            "constructor": "${{jobs.create-rai-job.outputs.rai_insights_dashboard}}",
+            "insight_1": "${{jobs.erroranalysis-rai-job.outputs.error_analysis}}",
+        }
+        gather_outputs = {"dashboard": None, "ux_json": None}
+        gather_job = ComponentJob(
+            component=f"RAIInsightsGather:{version_string}",
+            inputs=gather_inputs,
+            outputs=gather_outputs,
+        )
+
+        # Pipeline to construct the RAI Insights
+        insights_pipeline_job = PipelineJob(
+            experiment_name=f"ErrorAnalysis_Regression_All_Args_{version_string}",
+            description="Check regression example with all arguments",
             jobs={
                 "fetch-model-job": fetch_job,
                 "create-rai-job": create_rai_job,
