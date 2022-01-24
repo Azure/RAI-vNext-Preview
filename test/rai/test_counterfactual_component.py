71f6e51b7cefe07cc5828164ba8ba2c4ff34c562
Richard Edgar <riedgar@microsoft.com>
2022-01-24 07:29:06 -0500

    Extra testing of regression scenario (#17)
    Add individual tests for the causal, counterfactual and error analysis components which exercise all of the available arguments for the regression scenario. Explanations are skipped since there are no user facing arguments.
    
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/rai/test_counterfactual_component.py
+++ test/rai/test_counterfactual_component.py
@@ -85,7 +85,93 @@ class TestCounterfactualComponent:
         # Pipeline to construct the RAI Insights
         insights_pipeline_job = PipelineJob(
             experiment_name=f"Counterfactual_Classification_All_Args_{version_string}",
-            description="Expected failure due to multiple tool instances",
+            description="Test counterfactual component with all arguments",
+            jobs={
+                "fetch-model-job": fetch_job,
+                "create-rai-job": create_rai_job,
+                "counterfactual-rai-job": counterfactual_job,
+                "gather-job": gather_job,
+            },
+            inputs=pipeline_inputs,
+            outputs=None,
+            compute="cpucluster",
+        )
+
+        # Send it
+        insights_pipeline_job = submit_and_wait(ml_client, insights_pipeline_job)
+        assert insights_pipeline_job is not None
+
+    def test_regression_all_args(
+        self, ml_client: MLClient, component_config, registered_boston_model_id: str
+    ):
+        version_string = component_config["version"]
+
+        # Pipeline globals
+        pipeline_inputs = {
+            "target_column_name": "y",
+            "my_training_data": JobInput(dataset=f"Boston_Train_PQ:{version_string}"),
+            "my_test_data": JobInput(dataset=f"Boston_Test_PQ:{version_string}"),
+        }
+
+        # The job to fetch the model
+        fetch_job_inputs = {"model_id": registered_boston_model_id}
+        fetch_job_outputs = {"model_info_output_path": None}
+        fetch_job = ComponentJob(
+            component=f"FetchRegisteredModel:{version_string}",
+            inputs=fetch_job_inputs,
+            outputs=fetch_job_outputs,
+        )
+
+        # Top level RAI Insights component
+        create_rai_inputs = {
+            "title": "Run built from Python",
+            "task_type": "regression",
+            "model_info_path": "${{jobs.fetch-model-job.outputs.model_info_output_path}}",
+            "train_dataset": "${{inputs.my_training_data}}",
+            "test_dataset": "${{inputs.my_test_data}}",
+            "target_column_name": "${{inputs.target_column_name}}",
+            "categorical_column_names": "[]",
+        }
+        create_rai_outputs = {"rai_insights_dashboard": None}
+        create_rai_job = ComponentJob(
+            component=f"RAIInsightsConstructor:{version_string}",
+            inputs=create_rai_inputs,
+            outputs=create_rai_outputs,
+        )
+
+        # Setup counterfactual
+        counterfactual_inputs = {
+            "rai_insights_dashboard": "${{jobs.create-rai-job.outputs.rai_insights_dashboard}}",
+            "total_CFs": "10",
+            "method": "kdtree",
+            "desired_range": "[20, 100]",
+            "permitted_range": '{"ZN": [0, 10], "AGE": [0, 50], "CRIM": [25, 50], "INDUS": [0, 10]}',
+            "features_to_vary": '["ZN", "AGE", "CRIM", "INDUS"]',
+            "feature_importance": "True",
+        }
+        counterfactual_outputs = {"counterfactual": None}
+        counterfactual_job = ComponentJob(
+            component=f"RAIInsightsCounterfactual:{version_string}",
+            inputs=counterfactual_inputs,
+            outputs=counterfactual_outputs,
+        )
+
+        # Configure the gather component
+        gather_inputs = {
+            "constructor": "${{jobs.create-rai-job.outputs.rai_insights_dashboard}}",
+            "insight_1": "${{jobs.counterfactual-rai-job.outputs.counterfactual}}",
+        }
+        gather_outputs = {"dashboard": None, "ux_json": None}
+        gather_job = ComponentJob(
+            component=f"RAIInsightsGather:{version_string}",
+            inputs=gather_inputs,
+            outputs=gather_outputs,
+        )
+
+        # Pipeline to construct the RAI Insights
+        insights_pipeline_job = PipelineJob(
+            experiment_name=f"Counterfactual_Regression_All_Args_{version_string}",
+            description="Check regression example with all arguments",
             jobs={
                 "fetch-model-job": fetch_job,
                 "create-rai-job": create_rai_job,
