f973ff768ee1ecb0ac20452a00867f2b9e5e575e
Richard Edgar <riedgar@microsoft.com>
2022-01-27 13:25:40 -0500

    Helper components for Tabular Dataset (#21)
    We need to have some sort of support for `TabularDataset` even before the official DPv2 support. Workaround by providing a component which turns a `TabularDataset` into a Parquet file using SDKv1.
    
    For testing purposes, there is also a component to perform the opposite operation, reading in a parquet file and registering a `TabularDataset` from it.
    
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

---
+++ test/components/src_reg_tabular/reg_tabular.py
@@ -0,0 +1,82 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+import argparse
+import logging
+import time
+
+import pandas as pd
+
+from azureml.core import Dataset, Run
+
+_logger = logging.getLogger(__file__)
+logging.basicConfig(level=logging.INFO)
+
+
+def parse_args():
+    # setup arg parser
+    parser = argparse.ArgumentParser()
+
+    # add arguments
+    parser.add_argument("--dataset_input_path", type=str, help="Path to input model")
+    parser.add_argument(
+        "--dataset_base_name", type=str, help="Name of the registered tabular dataset"
+    )
+    parser.add_argument(
+        "--dataset_name_suffix", type=int, help="Set negative to use epoch_secs"
+    )
+
+    # parse args
+    args = parser.parse_args()
+
+    # return args
+    return args
+
+
+def load_dataset(parquet_path: str):
+    _logger.info("Loading parquet file: {0}".format(parquet_path))
+    df = pd.read_parquet(parquet_path)
+    print(df.dtypes)
+    print(df.head(10))
+    return df
+
+
+def main(args):
+    current_run = Run.get_context()
+    ws = current_run.experiment.workspace
+
+    datastore = ws.get_default_datastore()
+
+    print("Loading dataset")
+    df = load_dataset(args.dataset_input_path)
+
+    if args.dataset_name_suffix < 0:
+        suffix = int(time.time())
+    else:
+        suffix = args.dataset_name_suffix
+    registered_name = "{0}_{1}".format(args.dataset_base_name, suffix)
+    print(f"Registering dataset as {registered_name}")
+
+    datastore_path = (datastore, registered_name)
+    tabular_dataset = Dataset.Tabular.register_pandas_dataframe(
+        df, datastore_path, registered_name
+    )
+    _logger.info("Registered Dataset: {}".format(tabular_dataset))
+
+
+# run script
+if __name__ == "__main__":
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
+
+    # parse args
+    args = parse_args()
+
+    # run main function
+    main(args)
+
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
