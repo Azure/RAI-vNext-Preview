0e07d032b54fc5c9d868648db31cf8f9c7ef13e5
Richard Edgar <riedgar@microsoft.com>
2022-02-04 13:46:40 -0500

    Rethinking
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- src/responsibleai/rai_analyse/gather_rai_insights.py
+++ src/responsibleai/rai_analyse/gather_rai_insights.py
@@ -58,78 +58,74 @@ def main(args):
     dashboard_info = load_dashboard_info_file(args.constructor)
     _logger.info("Constructor info: {0}".format(dashboard_info))
 
-    with tempfile.TemporaryDirectory() as temp_rai_dir:
-        with tempfile.TemporaryDirectory() as incoming_temp_dir:
-            my_run = Run.get_context()
-            rai_temp = create_rai_insights_from_port_path(my_run, args.constructor)
-            rai_temp.save(temp_rai_dir)
+    with tempfile.TemporaryDirectory() as incoming_temp_dir:
+        incoming_dir = Path(incoming_temp_dir)
 
-            incoming_dir = Path(incoming_temp_dir)
-            shutil.copytree(temp_rai_dir, incoming_dir, dirs_exist_ok=True)
-            _logger.info("Copied RAI Insights input to temporary directory")
+        my_run = Run.get_context()
+        rai_temp = create_rai_insights_from_port_path(my_run, args.constructor)
+        rai_temp.save(incoming_temp_dir)
+        create_rai_tool_directories(incoming_dir)
+        _logger.info("Saved empty RAI Insights input to temporary directory")
 
-            create_rai_tool_directories(incoming_dir)
-            _logger.info("Copied empty RAIInsights")
+        insight_paths = [
+            args.insight_1,
+            args.insight_2,
+            args.insight_3,
+            args.insight_4,
+        ]
 
-            insight_paths = [
-                args.insight_1,
-                args.insight_2,
-                args.insight_3,
-                args.insight_4,
-            ]
+        included_tools: Dict[str, bool] = {
+            RAIToolType.CAUSAL: False,
+            RAIToolType.COUNTERFACTUAL: False,
+            RAIToolType.ERROR_ANALYSIS: False,
+            RAIToolType.EXPLANATION: False,
+        }
+        for i in range(len(insight_paths)):
+            current_insight_arg = insight_paths[i]
+            if current_insight_arg is not None:
+                current_insight_path = Path(current_insight_arg)
+                _logger.info("Checking dashboard info")
+                insight_info = load_dashboard_info_file(current_insight_path)
+                _logger.info("Insight info: {0}".format(insight_info))
 
-            included_tools: Dict[str, bool] = {
-                RAIToolType.CAUSAL: False,
-                RAIToolType.COUNTERFACTUAL: False,
-                RAIToolType.ERROR_ANALYSIS: False,
-                RAIToolType.EXPLANATION: False,
-            }
-            for i in range(len(insight_paths)):
-                current_insight_arg = insight_paths[i]
-                if current_insight_arg is not None:
-                    current_insight_path = Path(current_insight_arg)
-                    _logger.info("Checking dashboard info")
-                    insight_info = load_dashboard_info_file(current_insight_path)
-                    _logger.info("Insight info: {0}".format(insight_info))
+                # Cross check against the constructor
+                if insight_info != dashboard_info:
+                    err_string = _DASHBOARD_CONSTRUCTOR_MISMATCH.format(i + 1)
+                    raise ValueError(err_string)
 
-                    # Cross check against the constructor
-                    if insight_info != dashboard_info:
-                        err_string = _DASHBOARD_CONSTRUCTOR_MISMATCH.format(i + 1)
-                        raise ValueError(err_string)
+                # Copy the data
+                _logger.info("Copying insight {0}".format(i + 1))
+                tool = copy_insight_to_raiinsights(
+                    incoming_dir, current_insight_path
+                )
 
-                    # Copy the data
-                    _logger.info("Copying insight {0}".format(i + 1))
-                    tool = copy_insight_to_raiinsights(
-                        incoming_dir, current_insight_path
-                    )
+                # Can only have one instance of each tool
+                if included_tools[tool]:
+                    err_string = _DUPLICATE_TOOL.format(i + 1, tool)
+                    raise ValueError(err_string)
 
-                    # Can only have one instance of each tool
-                    if included_tools[tool]:
-                        err_string = _DUPLICATE_TOOL.format(i + 1, tool)
-                        raise ValueError(err_string)
+                included_tools[tool] = True
+            else:
+                _logger.info("Insight {0} is None".format(i + 1))
 
-                    included_tools[tool] = True
-                else:
-                    _logger.info("Insight {0} is None".format(i + 1))
+        _logger.info("Tool summary: {0}".format(included_tools))
 
-            _logger.info("Tool summary: {0}".format(included_tools))
+        rai_i = RAIInsights.load(incoming_dir)
+        _logger.info("Object loaded")
 
-            rai_i = RAIInsights.load(incoming_dir)
-            _logger.info("Object loaded")
+        rai_i.save(args.dashboard)
+        _logger.info("Saved dashboard to oputput")
 
-            rai_i.save(args.dashboard)
-            _logger.info("Saved dashboard to oputput")
+        rai_data = rai_i.get_data()
+        rai_dict = serialize_json_safe(rai_data)
+        json_filename = "dashboard.json"
+        output_path = Path(args.ux_json) / json_filename
+        with open(output_path, "w") as json_file:
+            json.dump(rai_dict, json_file)
+        _logger.info("Dashboard JSON written")
 
-            rai_data = rai_i.get_data()
-            rai_dict = serialize_json_safe(rai_data)
-            json_filename = "dashboard.json"
-            output_path = Path(args.ux_json) / json_filename
-            with open(output_path, "w") as json_file:
-                json.dump(rai_dict, json_file)
-            _logger.info("Dashboard JSON written")
-
-            add_properties_to_gather_run(dashboard_info, included_tools)
-            _logger.info("Processing completed")
+        add_properties_to_gather_run(dashboard_info, included_tools)
+        _logger.info("Processing completed")
 
 
 # run script
