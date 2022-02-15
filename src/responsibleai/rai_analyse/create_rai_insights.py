ef2c34e987e22b39bb5559fcb78c29df4eba99af
Richard Edgar <riedgar@microsoft.com>
2022-02-15 11:39:58 -0500

    Correcting load path
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- src/responsibleai/rai_analyse/create_rai_insights.py
+++ src/responsibleai/rai_analyse/create_rai_insights.py
@@ -106,7 +106,7 @@ def main(args):
         download_model_to_dir(
             my_run.experiment.workspace, model_id, unwrapped_model_dir
         )
-        with DeployedModel(unwrapped_model_dir) as dm:
+        with DeployedModel(os.path.join(unwrapped_model_dir, model_id)) as dm:
             _logger.info("Model Deployed")
             time.sleep(60)
             _logger.info("Calling endpoint")
