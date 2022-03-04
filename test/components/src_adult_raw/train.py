b1f16db24950384c92b29599574e2a5fcff9b5d8
Richard Edgar <riedgar@microsoft.com>
2022-03-04 15:36:51 -0500

    Drafting training script for raw adult census
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

---
+++ test/components/src_adult_raw/train.py
@@ -0,0 +1,122 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+
+import argparse
+import tempfile
+import os
+import shutil
+
+import mlflow
+import mlflow.sklearn
+
+import pandas as pd
+
+from sklearn import svm
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import make_column_selector as selector
+from sklearn.pipeline import Pipeline
+
+from azureml.core import Run
+
+target_column_name = "income"
+
+
+def parse_args():
+    # setup arg parser
+    parser = argparse.ArgumentParser()
+
+    # add arguments
+    parser.add_argument("--training_data", type=str, help="Path to training data")
+    parser.add_argument("--model_output", type=str, help="Path of output model")
+
+    # parse args
+    args = parser.parse_args()
+
+    # return args
+    return args
+
+
+def load_data(args) -> pd.DataFrame:
+    print("Reading data")
+    all_data = pd.read_parquet(args.training_data)
+    print(all_data.dtypes)
+    return all_data
+
+
+def train_model(all_data: pd.DataFrame):
+    y = (all_data[target_column_name] == ">50K") * 1
+    X = all_data.drop(labels=[target_column_name], axis=1)
+
+    numeric_transformer = Pipeline(
+        steps=[
+            ("impute", SimpleImputer()),
+            ("scaler", StandardScaler()),
+        ]
+    )
+
+    categorical_transformer = Pipeline(
+        [
+            ("impute", SimpleImputer(strategy="most_frequent")),
+            ("ohe", OneHotEncoder(handle_unknown="ignore", sparse=False)),
+        ]
+    )
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", numeric_transformer, selector(dtype_exclude="category")),
+            ("cat", categorical_transformer, selector(dtype_include="category")),
+        ]
+    )
+
+    model_pipeline = Pipeline(steps=[("preprocess", preprocessor), ("SVM", svm.SVC())])
+
+    model_pipeline.fit(X, y)
+    return model_pipeline
+
+
+def main(args):
+    current_experiment = Run.get_context().experiment
+    tracking_uri = current_experiment.workspace.get_mlflow_tracking_uri()
+    print("tracking_uri: {0}".format(tracking_uri))
+    mlflow.set_tracking_uri(tracking_uri)
+    mlflow.set_experiment(current_experiment.name)
+
+    all_data = load_data(args)
+    sklearn_model = train_model(all_data)
+
+    # Saving model with mlflow
+    with tempfile.TemporaryDirectory() as td:
+        print("Saving model with MLFlow to temporary directory")
+        tmp_output_dir = os.path.join(td, "my_model_dir")
+        mlflow.sklearn.save_model(sk_model=sklearn_model, path=tmp_output_dir)
+
+        print("Copying MLFlow model to output path")
+        for file_name in os.listdir(tmp_output_dir):
+            print("  Copying: ", file_name)
+            # As of Python 3.8, copytree will acquire dirs_exist_ok as
+            # an option, removing the need for listdir
+            shutil.copy2(
+                src=os.path.join(tmp_output_dir, file_name),
+                dst=os.path.join(args.model_output, file_name),
+            )
+
+
+# run script
+if __name__ == "__main__":
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
+
+    # parse args
+    args = parse_args()
+
+    # run main function
+    main(args)
+
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
