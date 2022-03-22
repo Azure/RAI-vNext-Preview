3df26cf06d621b1407d31031ebc31589184984b3
Richard Edgar <riedgar@microsoft.com>
2022-03-22 10:23:26 -0400

    Eliminate another input
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/rai/test_tabular_datasets.py
+++ test/rai/test_tabular_datasets.py
@@ -145,7 +145,7 @@ class TestRegisterTabularDataset:
 
             rai_gather_job = rai_gather_component(
                 constructor=construct_job.outputs.rai_insights_dashboard,
-                insight_1=rai_explanation_job.outputs.explanation,
+                #insight_1=rai_explanation_job.outputs.explanation,
                 #insight_2=None,
                 #insight_3=None,
                 #insight_4=None,
