47b1303ba51cc374cfddc534c417ae6983b7ed56
Richard Edgar <riedgar@microsoft.com>
2022-03-23 09:23:07 -0400

    Convert fixture
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/conftest.py
+++ test/conftest.py
@@ -99,59 +99,39 @@ def registered_boston_model_id(ml_client, component_config):
     model_name_suffix = int(time.time())
     model_name = "common_fetch_model_boston"
 
-    # Configure the global pipeline inputs:
-    pipeline_inputs = {
-        "target_column_name": "y",
-        "my_training_data": JobInput(
-            type="uri_file", path=f"azureml:Boston_Train_PQ:{version_string}"
-        ),
-        "my_test_data": JobInput(
-            type="uri_file", path=f"azureml:Boston_Test_PQ:{version_string}"
-        ),
-    }
-
-    # Specify the training job
-    train_job_inputs = {
-        "target_column_name": "${{parent.inputs.target_column_name}}",
-        "training_data": "${{parent.inputs.my_training_data}}",
-        "categorical_features": "[]",
-        "continuous_features": '["CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE","DIS", "RAD", "TAX", "PTRATIO", "B", "LSTAT"]',
-    }
-    train_job_outputs = {"model_output": None}
-    train_job = CommandComponent(
-        component=f"azureml:TrainBostonForRAI:{version_string}",
-        inputs=train_job_inputs,
-        outputs=train_job_outputs,
+    train_component = load_component(
+        client=ml_client, name="TrainBostonForRAI", version=version_string
     )
-
-    # The model registration job
-    register_job_inputs = {
-        "model_input_path": "${{parent.jobs.train-model-job.outputs.model_output}}",
-        "model_base_name": model_name,
-        "model_name_suffix": model_name_suffix,
-    }
-    register_job_outputs = {"model_info_output_path": None}
-    register_job = CommandComponent(
-        component=f"azureml:RegisterModel:{version_string}",
-        inputs=register_job_inputs,
-        outputs=register_job_outputs,
+    register_component = load_component(
+        client=ml_client, name="RegisterModel", version=version_string
     )
-    # Assemble into a pipeline
-    register_pipeline = PipelineJob(
-        experiment_name=f"Register_Boston_Model_Fixture_{version_string}",
-        description="Python submitted Boston model registration",
-        jobs={
-            "train-model-job": train_job,
-            "register-model-job": register_job,
-        },
-        inputs=pipeline_inputs,
-        outputs=register_job_outputs,
-        compute="azureml:cpucluster",
-    )
+    boston_train_pq = JobInput(path=f"Boston_Train_PQ:{version_string}")
 
-    # Send it
-    register_pipeline_job = submit_and_wait(ml_client, register_pipeline)
-    assert register_pipeline_job is not None
+    @dsl.pipeline(
+        compute="cpucluster",
+        description="Register Common Model for Boston",
+        experiment_name="Fixture_Common_Boston_Model",
+    )
+    def my_training_pipeline(target_column_name, training_data):
+        trained_model = train_component(
+            target_column_name=target_column_name,
+            training_data=training_data,
+            categorical_features="[]",
+            continuous_features='["CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE","DIS", "RAD", "TAX", "PTRATIO", "B", "LSTAT"]',
+        )
+
+        _ = register_component(
+            model_input_path=trained_model.outputs.model_output,
+            model_base_name=model_name,
+            model_name_suffix=model_name_suffix,
+        )
+
+        return {}
+
+    training_pipeline = my_training_pipeline("income", boston_train_pq)
+
+    training_pipeline_job = submit_and_wait(ml_client, training_pipeline)
+    assert training_pipeline_job is not None
 
     expected_model_id = f"{model_name}_{model_name_suffix}:1"
     return expected_model_id
