0304be2f0216ec872eb6676698ea035850eb25d1
Richard Edgar <riedgar@microsoft.com>
2022-03-24 09:52:49 -0400

    Convert the other test
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/rai/test_rai_gather_errors.py
+++ test/rai/test_rai_gather_errors.py
@@ -104,8 +104,8 @@ class TestRAIGatherErrors:
                 feature_importance=True,  # Should be default
             )
 
-            # Now a single gather components
-            gather_job = rai_components.gather(
+            # Now a single gather component
+            gather_job = rai_components.rai_gather(
                 constructor=construct1_job.outputs.rai_insights_dashboard,
                 insight_1=causal1_job.outputs.causal,
                 insight_2=counterfactual2_job.outputs.counterfactual,
@@ -133,95 +133,73 @@ class TestRAIGatherErrors:
         assert pipeline_job is not None
 
     def test_multiple_tool_instances(
-        self, ml_client: MLClient, component_config, registered_adult_model_id: str
+        self,
+        ml_client: MLClient,
+        component_config,
+        registered_adult_model_id: str,
+        rai_components,
     ):
         version_string = component_config["version"]
 
-        # Pipeline globals
-        pipeline_inputs = {
-            "target_column_name": "income",
-            "my_training_data": JobInput(dataset=f"Adult_Train_PQ:{version_string}"),
-            "my_test_data": JobInput(dataset=f"Adult_Test_PQ:{version_string}"),
-        }
-
-        # The job to fetch the model
-        fetch_job_inputs = {"model_id": registered_adult_model_id}
-        fetch_job_outputs = {"model_info_output_path": None}
-        fetch_job = CommandComponent(
-            component=f"FetchRegisteredModel:{version_string}",
-            inputs=fetch_job_inputs,
-            outputs=fetch_job_outputs,
-        )
-
-        # Top level RAI Insights component
-        create_rai_inputs = {
-            "title": "Run built from Python",
-            "task_type": "classification",
-            "model_info_path": "${{jobs.fetch-model-job.outputs.model_info_output_path}}",
-            "train_dataset": "${{inputs.my_training_data}}",
-            "test_dataset": "${{inputs.my_test_data}}",
-            "target_column_name": "${{inputs.target_column_name}}",
-            "categorical_column_names": '["Race", "Sex", "Workclass", "Marital Status", "Country", "Occupation"]',
-        }
-        create_rai_outputs = {"rai_insights_dashboard": None}
-        create_rai_job = CommandComponent(
-            component=f"RAIInsightsConstructor:{version_string}",
-            inputs=create_rai_inputs,
-            outputs=create_rai_outputs,
-        )
-
-        # Setup two causal analyses
-        causal_inputs = {
-            "rai_insights_dashboard": "${{jobs.create-rai-job.outputs.rai_insights_dashboard}}",
-            "treatment_features": '["Age", "Sex"]',
-            "heterogeneity_features": '["Marital Status"]',
-        }
-        causal_outputs = {"causal": None}
-        causal_job_01 = CommandComponent(
-            component=f"RAIInsightsCausal:{version_string}",
-            inputs=causal_inputs,
-            outputs=causal_outputs,
-        )
-
-        causal_inputs["treatment_cost"] = "[0.01, 0.02]"
-        causal_job_02 = CommandComponent(
-            component=f"RAIInsightsCausal:{version_string}",
-            inputs=causal_inputs,
-            outputs=causal_outputs,
-        )
-
-        # Configure the gather component
-        gather_inputs = {
-            "constructor": "${{jobs.create-rai-job.outputs.rai_insights_dashboard}}",
-            "insight_1": "${{jobs.causal-rai-job-01.outputs.causal}}",
-            "insight_2": "${{jobs.causal-rai-job-02.outputs.causal}}",
-        }
-        gather_outputs = {"dashboard": None, "ux_json": None}
-        gather_job = CommandComponent(
-            component=f"RAIInsightsGather:{version_string}",
-            inputs=gather_inputs,
-            outputs=gather_outputs,
-        )
-
-        # Pipeline to construct the RAI Insights
-        insights_pipeline_job = PipelineJob(
-            experiment_name=f"XFAIL_multiple_tool_instances_{version_string}",
-            description="Expected failure due to multiple tool instances",
-            jobs={
-                "fetch-model-job": fetch_job,
-                "create-rai-job": create_rai_job,
-                "causal-rai-job-01": causal_job_01,
-                "causal-rai-job-02": causal_job_02,
-                "gather-job": gather_job,
-            },
-            inputs=pipeline_inputs,
-            outputs=None,
+        @dsl.pipeline(
             compute="cpucluster",
+            description="Test Error Analysis component with all arguments",
+            experiment_name=f"TestRAIGatherErrors_test_multiple_tool_instances_{version_string}",
+        )
+        def test_multiple_tool_instances(
+            target_column_name,
+            train_data,
+            test_data,
+        ):
+            fetch_model_job = rai_components.fetch_model(
+                model_id=registered_adult_model_id
+            )
+
+            construct_job = rai_components.rai_constructor(
+                title="Run built from DSL",
+                task_type="classification",
+                model_info_path=fetch_model_job.outputs.model_info_output_path,
+                train_dataset=train_data,
+                test_dataset=test_data,
+                target_column_name=target_column_name,
+                categorical_column_names='["Race", "Sex", "Workclass", "Marital Status", "Country", "Occupation"]',
+                maximum_rows_for_test_dataset=5000,  # Should be default
+                classes="[]",  # Should be default
+            )
+
+            explain1_job = rai_components.rai_explanation(
+                comment="explain1_job",
+                rai_insights_dashboard=create_rai_job.outputs.rai_insights_dashboard,
+            )
+
+            explain2_job = rai_components.rai_explanation(
+                comment="explain2_job",
+                rai_insights_dashboard=create_rai_job.outputs.rai_insights_dashboard,
+            )
+
+            gather_job = rai_components.rai_gather(
+                constructor=construct_job.outputs.rai_insights_dashboard,
+                insight_1=explain1_job.outputs.explanation,
+                insight_2=explain2_job.outputs.explanation,
+            )
+
+            gather_job.outputs.dashboard.mode = "upload"
+            gather_job.outputs.ux_json.mode = "upload"
+
+            return {
+                "dashboard": gather_job.outputs.dashboard,
+                "ux_json": gather_job.outputs.ux_json,
+            }
+
+        # Assemble into a pipeline
+        pipeline_job = test_multiple_tool_instances(
+            target_column_name="income",
+            train_data=JobInput(path=f"Adult_Train_PQ:{version_string}"),
+            test_data=JobInput(path=f"Adult_Test_PQ:{version_string}"),
         )
 
         # Send it
-        insights_pipeline_job = submit_and_wait(
-            ml_client, insights_pipeline_job, "Failed"
-        )
-        assert insights_pipeline_job is not None
-        # Unfortunately we can't check anything else right now
+        pipeline_job = submit_and_wait(ml_client, pipeline_job, "Failed")
+        # Want to do more here, but there isn't anything useful in the returned job
+        # Problem is, the job might have failed for some other reason
+        assert pipeline_job is not None
