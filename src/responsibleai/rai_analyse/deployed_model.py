0f7bf423eaf324a62f362df4d657866181743d19
Richard Edgar <riedgar@microsoft.com>
2022-03-28 10:37:48 -0400

    Improving readiness check
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- src/responsibleai/rai_analyse/deployed_model.py
+++ src/responsibleai/rai_analyse/deployed_model.py
@@ -15,6 +15,20 @@ _logger = logging.getLogger(__file__)
 logging.basicConfig(level=logging.INFO)
 
 
+def check_for_ready_string(log_output: str):
+    ready_strings = [
+        "Serving on http://127",
+        "Booting worker with pid",
+        "Listening at: http://127.0.0.1:5000",
+    ]
+
+    result = False
+    for rs in ready_strings:
+        if rs in log_output:
+            result = True
+    return result
+
+
 class DeployedModel:
     def __init__(self, model_dir: str):
         self._target_model_dir = model_dir
@@ -36,8 +50,7 @@ class DeployedModel:
 
         for line in self._server.stdout:
             _logger.info(line.strip())
-            target_string = "Serving on http://127" # "Booting worker with pid" # "Listening at: http://127.0.0.1:5000"
-            if target_string in line:
+            if check_for_ready_string(line):
                 break
             if self._server.returncode is not None:
                 # Server has crashed
@@ -56,7 +69,7 @@ class DeployedModel:
 
     def _call_model_and_extract(self, input_df: pd.DataFrame, target: str):
         payload = input_df.to_json(orient="split")
-        #_logger.info("Payload: {0}".format(payload))
+        # _logger.info("Payload: {0}".format(payload))
         headers = {"Content-Type": "application/json"}
         r = requests.post(
             "http://127.0.0.1:5000/invocations",
@@ -64,13 +77,13 @@ class DeployedModel:
             data=payload,
             timeout=100,
         )
-        #_logger.info("Call to model completed: {0}".format(r.text))
+        # _logger.info("Call to model completed: {0}".format(r.text))
         decoded = json.loads(r.text)
-        #_logger.info(f"Decoded response: {decoded}")
+        # _logger.info(f"Decoded response: {decoded}")
         return decoded[target]
 
     def predict(self, input_df: pd.DataFrame):
-        return self._call_model_and_extract(input_df, 'pred')
+        return self._call_model_and_extract(input_df, "pred")
 
     def predict_proba(self, input_df: pd.DataFrame):
-        return self._call_model_and_extract(input_df, 'pred_proba')
+        return self._call_model_and_extract(input_df, "pred_proba")
