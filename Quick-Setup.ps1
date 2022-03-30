e222ca7feeed477f39daa03a952a4335c0e22104
Richard Edgar <riedgar@microsoft.com>
2022-03-30 08:39:49 -0400

    Working on script
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- Quick-Setup.ps1
+++ Quick-Setup.ps1
@@ -31,8 +31,24 @@ function Create-ComponentConfigJson(
 }
 
 
-Write-Host "Creating conda environment '$EnvName' with python v3.8"
+Write-Host "=-= Creating conda environment '$EnvName' with python v3.8"
+conda create -y -n $EnvName python=3.8
+conda activate $EnvName
 
+Write-Host "=-= Installing Jupyter"
+pip install jupyter markupsafe<=2.0.1 itsdangerous==2.0.1
+
+Write-Host "=-= Installing responsibleai"
+pip install responsibleai~=0.17.0 raiwidgets~=0.17.0 pyarrow
+
+Write-Host "=-= Installing mini SDK"
+pip install -e src/azure-ml-rai
+
+Write-Host "=-= Installing other requirements"
+pip install -r requirements-dev-releasepackage.txt
+
+Write-Host "=-= Creating workspace config JSON"
 Create-ConfigJson $SubId $ResourceGroup $Workspace
 
+Write-Host "=-= Creating component config JSON"
 Create-ComponentConfigJson -version 1
