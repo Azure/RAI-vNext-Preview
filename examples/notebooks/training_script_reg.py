687fac698f113fed47795de1d9ec0359526d0a59
Ilya Matiach <ilmat@microsoft.com>
2022-04-09 10:20:11 -0400

    add orange regression example

---
+++ examples/notebooks/training_script_reg.py
@@ -0,0 +1,82 @@
+
+import argparse
+import os
+import shutil
+import tempfile
+
+
+from azureml.core import Run
+
+import mlflow
+import mlflow.sklearn
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+def parse_args():
+    # setup arg parser
+    parser = argparse.ArgumentParser()
+
+    # add arguments
+    parser.add_argument("--training_data", type=str, help="Path to training data")
+    parser.add_argument("--target_column_name", type=str, help="Name of target column")
+    parser.add_argument("--model_output", type=str, help="Path of output model")
+
+    # parse args
+    args = parser.parse_args()
+
+    # return args
+    return args
+
+
+def main(args):
+    current_experiment = Run.get_context().experiment
+    tracking_uri = current_experiment.workspace.get_mlflow_tracking_uri()
+    print("tracking_uri: {0}".format(tracking_uri))
+    mlflow.set_tracking_uri(tracking_uri)
+    mlflow.set_experiment(current_experiment.name)
+
+    # Read in data
+    print("Reading data")
+    all_data = pd.read_parquet(args.training_data)
+
+    print("Extracting X_train, y_train")
+    print("all_data cols: {0}".format(all_data.columns))
+    y_train = all_data[args.target_column_name]
+    X_train = all_data.drop(labels=args.target_column_name, axis="columns")
+    print("X_train cols: {0}".format(X_train.columns))
+
+    print("Training model")
+    # The estimator can be changed to suit
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    # Saving model with mlflow - leave this section unchanged
+    with tempfile.TemporaryDirectory() as td:
+        print("Saving model with MLFlow to temporary directory")
+        tmp_output_dir = os.path.join(td, "my_model_dir")
+        mlflow.sklearn.save_model(sk_model=model, path=tmp_output_dir)
+
+        print("Copying MLFlow model to output path")
+        for file_name in os.listdir(tmp_output_dir):
+            print("  Copying: ", file_name)
+            # As of Python 3.8, copytree will acquire dirs_exist_ok as
+            # an option, removing the need for listdir
+            shutil.copy2(src=os.path.join(tmp_output_dir, file_name), dst=os.path.join(args.model_output, file_name))
+
+
+# run script
+if __name__ == "__main__":
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
+
+    # parse args
+    args = parse_args()
+
+    # run main function
+    main(args)
+
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
