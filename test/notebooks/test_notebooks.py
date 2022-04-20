d6f02e6b994c86d193ed06647e89821123eddb5d
Richard Edgar <riedgar@microsoft.com>
2022-04-20 14:47:26 -0400

    Add build pipeline to test notebooks (#54)
    Add testing for notebooks.
    
    - Refactor `requirements` files slightly
    - Add section to builds to run notebook tests
    - Add notebook test. This needs to process the notebook to pick the right component version
    
    Use of the miniSDK was trimmed from the notebook as well; it should be superseded by the built-in functionality shortly.
    
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

---
+++ test/notebooks/test_notebooks.py
@@ -0,0 +1,52 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+import nbformat as nbf
+import papermill as pm
+import pytest
+import time
+
+from typing import Dict
+
+
+def update_cells(input_nb_path, output_nb_path, replacement_strings: Dict[str, str]):
+    notebook = nbf.read(input_nb_path, as_version=nbf.NO_CONVERT)
+
+    for cell in notebook["cells"]:
+        for original, update in replacement_strings.items():
+            if cell["source"] == original:
+                cell["source"] = update
+
+    nbf.write(notebook, output_nb_path)
+
+
+def assay_one_notebook(notebook_name, test_values, replacement_strings: Dict[str, str]):
+    """Test a single notebook.
+    This uses nbformat to replace the contents of given cells for use in automated pipelines
+    Makes certain assumptions about directory layout.
+    """
+    input_notebook = "examples/notebooks/" + notebook_name + ".ipynb"
+    processed_notebook = "./test/notebooks/" + notebook_name + ".processed.ipynb"
+    output_notebook = "./test/notebooks/" + notebook_name + ".output.ipynb"
+
+    update_cells(input_notebook, processed_notebook, replacement_strings)
+    pm.execute_notebook(processed_notebook, output_notebook)
+
+
+@pytest.mark.notebooks
+def test_responsibleaidashboard_housing_classification_model_debugging(
+    component_config,
+):
+    nb_name = "responsibleaidashboard-housing-classification-model-debugging"
+
+    version_string = component_config["version"]
+    train_version_string = int(time.time())
+
+    replacements = dict()
+    replacements["version_string = '1'"] = f"version_string = '{version_string}'"
+    replacements[
+        "training_component_version_string = '4'"
+    ] = f"training_component_version_string = '{train_version_string}'"
+
+    assay_one_notebook(nb_name, dict(), replacements)
