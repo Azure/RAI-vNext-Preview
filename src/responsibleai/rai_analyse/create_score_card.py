075ebc2eb871fd1c07f13589202cadaab898074d
Gaurav Gupta <47334368+gaugup@users.noreply.github.com>
2023-03-29 16:50:25 -0700

    Update create_score_card.py

--- src/responsibleai/rai_analyse/create_score_card.py
+++ src/responsibleai/rai_analyse/create_score_card.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT License.
 
 import argparse
+from datetime import datetime
 import json
 import logging
 import os
@@ -178,9 +179,15 @@ def main(args):
             wsid,
         )
 
+        if "startTimeUtc" not in run_details:
+            # Get UTC from python datetime module if this is not available from run details
+            startTimeUtc = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+        else:
+            startTimeUtc = run_details["startTimeUtc"]
+
         config["runinfo"] = {
             "submittedBy": run_details["submittedBy"],
-            "startTimeUtc": run_details["startTimeUtc"],
+            "startTimeUtc": startTimeUtc,
             "dashboard_link": dashboard_link,
             "model_id": dashboard_info[DashboardInfo.RAI_INSIGHTS_MODEL_ID_KEY],
             "dashboard_title": dashboard_info[
