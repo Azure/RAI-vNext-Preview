6c1154c9d151821030e9ba6c4e7b02193d961401
root <kicha@microsoft.com>
2023-05-18 17:10:23 +0000

    Handle json parsing error.

--- src/responsibleai/rai_analyse/create_rai_insights.py
+++ src/responsibleai/rai_analyse/create_rai_insights.py
@@ -8,14 +8,13 @@ import os
 import shutil
 
 from _telemetry._loggerfactory import _LoggerFactory, track
-from arg_helpers import (boolean_parser,
-                         json_empty_is_none_parser)
+from arg_helpers import boolean_parser, json_empty_is_none_parser
 from azureml.core import Run
 from constants import DashboardInfo, PropertyKeyValues
 from rai_component_utilities import (default_json_handler, fetch_model_id,
-                                     get_test_dataset_id, get_train_dataset_id,
-                                     load_dataset, load_mlflow_model,
-                                     get_arg)
+                                     get_arg, get_test_dataset_id,
+                                     get_train_dataset_id, load_dataset,
+                                     load_mlflow_model)
 from raiutils.exceptions import UserConfigValidationException
 from responsibleai.feature_metadata import FeatureMetadata
 
--- src/responsibleai/rai_analyse/rai_component_utilities.py
+++ src/responsibleai/rai_analyse/rai_component_utilities.py
@@ -16,12 +16,11 @@ from typing import Any, Dict, Optional
 import mlflow
 import mltable
 import pandas as pd
+from arg_helpers import get_from_args
 from azureml.core import Model, Run, Workspace
 from constants import DashboardInfo, PropertyKeyValues, RAIToolType
 from raiutils.exceptions import UserConfigValidationException
 from responsibleai.feature_metadata import FeatureMetadata
-
-from arg_helpers import get_from_args
 
 from responsibleai import RAIInsights
 from responsibleai import __version__ as responsibleai_version
@@ -429,6 +428,9 @@ def get_arg(args, arg_name: str, custom_parser, allow_none: bool) -> Any:
     try:
         return get_from_args(args, arg_name, custom_parser, allow_none)
     except ValueError as e:
-        raise UserConfigError(f"Unable to parse {arg_name} from {args}. Please check that {extracted} is valid input and that {arg_name} exists."
+        raise UserConfigError(
+            f"Unable to parse {arg_name} from {args}."
+            f"Please check that {args} is valid input and that {arg_name} exists."
             "For example, a json string with unquoted string value or key can cause this error."
-            f"Raw parsing error: {e}")
+            f"Raw parsing error: {e}"
+        )
