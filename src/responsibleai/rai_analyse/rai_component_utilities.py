541c29d997eb21b548cf4e63f602f68882d10163
RubyZ10 <zhenzhu@microsoft.com>
2023-05-23 14:00:30 -0700

    fix invalid input

--- src/responsibleai/rai_analyse/rai_component_utilities.py
+++ src/responsibleai/rai_analyse/rai_component_utilities.py
@@ -67,7 +67,10 @@ def fetch_model_id(model_info_path: str):
     model_info_path = os.path.join(model_info_path, DashboardInfo.MODEL_INFO_FILENAME)
     with open(model_info_path, "r") as json_file:
         model_info = json.load(json_file)
-    return model_info[DashboardInfo.MODEL_ID_KEY]
+    if DashboardInfo.MODEL_ID_KEY not in model_info:
+        raise UserConfigError(f"Invalid input json, expecting key {DashboardInfo.MODEL_ID_KEY} exists in the input json")
+    else:
+        return model_info[DashboardInfo.MODEL_ID_KEY]
 
 
 def load_mlflow_model(
