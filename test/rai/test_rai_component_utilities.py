58630f0a59d89d4d6fbb54bc63b63f5b5130736b
RubyZ10 <zhenzhu@microsoft.com>
2023-05-31 13:30:49 -0700

    test import

--- test/rai/test_rai_component_utilities.py
+++ test/rai/test_rai_component_utilities.py
@@ -3,6 +3,8 @@ import os
 
 import pytest
 from raiutils.exceptions import UserConfigValidationException
+from src.responsibleai.rai_analyse.constants import DashboardInfo
+from src.responsibleai.rai_analyse.rai_component_utilities import fetch_model_id
 
 
 class TestFetchModelId:
@@ -10,9 +12,6 @@ class TestFetchModelId:
     @pytest.mark.skip(reason="Skipping this test for now due to import issues, will enable once fixed")
     @pytest.mark.parametrize("model_info_path, model_info, expected_model_id", [("", {"id": "abc"}, "abc")])
     def test_fetch_model_id(self, model_info_path, model_info, expected_model_id):
-        from src.responsibleai.rai_analyse.constants import DashboardInfo
-        from src.responsibleai.rai_analyse.rai_component_utilities import \
-            fetch_model_id
         model_info_path = os.path.join(model_info_path, DashboardInfo.MODEL_INFO_FILENAME)
         with open(model_info_path, "w") as json_file:
             json.dump(model_info, json_file)
@@ -21,9 +20,6 @@ class TestFetchModelId:
     @pytest.mark.skip(reason="Skipping this test for now due to import issues, will enable once fixed")
     @pytest.mark.parametrize("model_info_path, model_info", [("", {"otherKeys": "abc"})])
     def test_fetch_model_id_invalid_input(self, model_info_path, model_info):
-        from src.responsibleai.rai_analyse.constants import DashboardInfo
-        from src.responsibleai.rai_analyse.rai_component_utilities import \
-            fetch_model_id
         model_info_path = os.path.join(model_info_path, DashboardInfo.MODEL_INFO_FILENAME)
         with open(model_info_path, "w") as json_file:
             json.dump(model_info, json_file)
