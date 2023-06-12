02825322a8726468f42834e68b3761488cd1a527
kicha0 <kicha@microsoft.com>
2023-06-12 11:06:29 -0700

    Add import path in syspath. Enable TestFetchModelId (#185)
    * Add import path in syspath. Enable TestFetchModelId
    
    * fix unit test
    
    * fix lint
    
    * fix sort
    
    * add mlflow
    
    ---------
    
    Co-authored-by: RubyZ10 <zhenzhu@microsoft.com>

--- test/rai/test_rai_component_utilities.py
+++ test/rai/test_rai_component_utilities.py
@@ -4,30 +4,37 @@ import os
 import pytest
 from raiutils.exceptions import UserConfigValidationException
 
+from src.responsibleai.rai_analyse.constants import DashboardInfo
+from src.responsibleai.rai_analyse.rai_component_utilities import \
+    fetch_model_id
+
+
+@pytest.fixture
+def temp_dir(tmpdir):
+    # Create a temporary directory for testing
+    return str(tmpdir)
+
 
 class TestFetchModelId:
-
-    @pytest.mark.skip(reason="Skipping this test for now due to import issues, will enable once fixed")
-    @pytest.mark.parametrize("model_info_path, model_info, expected_model_id", [("", {"id": "abc"}, "abc")])
-    def test_fetch_model_id(self, model_info_path, model_info, expected_model_id):
-        from src.responsibleai.rai_analyse.constants import DashboardInfo
-        from src.responsibleai.rai_analyse.rai_component_utilities import \
-            fetch_model_id
-        model_info_path = os.path.join(model_info_path, DashboardInfo.MODEL_INFO_FILENAME)
+    @pytest.mark.parametrize(
+        "model_info", [
+            ({"id": "abc123"}),  # Valid input case
+            ({"id": 123}),  # Valid input case with integer model ID
+            ({"id": None})  # Valid input case with None model ID
+        ]
+    )
+    def test_fetch_model_id(self, temp_dir, model_info):
+        model_info_path = os.path.join(temp_dir, DashboardInfo.MODEL_INFO_FILENAME)
         with open(model_info_path, "w") as json_file:
             json.dump(model_info, json_file)
-        assert fetch_model_id(model_info_path) == expected_model_id
+        assert fetch_model_id(temp_dir) == model_info["id"]
 
-    @pytest.mark.skip(reason="Skipping this test for now due to import issues, will enable once fixed")
-    @pytest.mark.parametrize("model_info_path, model_info", [("", {"otherKeys": "abc"})])
-    def test_fetch_model_id_invalid_input(self, model_info_path, model_info):
-        from src.responsibleai.rai_analyse.constants import DashboardInfo
-        from src.responsibleai.rai_analyse.rai_component_utilities import \
-            fetch_model_id
-        model_info_path = os.path.join(model_info_path, DashboardInfo.MODEL_INFO_FILENAME)
+    @pytest.mark.parametrize("model_info", [({"otherKeys": "abc"})])
+    def test_fetch_model_id_invalid_input(self, temp_dir, model_info):
+        model_info_path = os.path.join(temp_dir, DashboardInfo.MODEL_INFO_FILENAME)
         with open(model_info_path, "w") as json_file:
             json.dump(model_info, json_file)
         with pytest.raises(
             UserConfigValidationException,
                 match=f"Invalid input, expecting key {DashboardInfo.MODEL_ID_KEY} to exist in the input json"):
-            fetch_model_id(model_info_path)
+            fetch_model_id(temp_dir)
